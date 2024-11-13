d87b6e753297edda8a2358e88b1240da52c1b2b2
Micaela <micaelaberzoni01@gmail.com>
2024-11-13 00:07:07 -0300

    avance 3 <?php require 'database.php'; // Incluye la conexión
    if (isset($_GET['paso_id'])) {
        $pasoId = $_GET['paso_id'];
    
        $query = "SELECT p.nombre_producto FROM productos p
                  JOIN producto_paso pp ON p.id_producto = pp.id_producto
                  JOIN pasos ps ON pp.id_paso = ps.id_pasos
                  WHERE ps.id_pasos = :pasoId";
    
        // Usar PDO para ejecutar la consulta
        $stmt = $pdo->prepare($query);
        $stmt->bindParam(':pasoId', $pasoId, PDO::PARAM_INT);
        $stmt->execute();
    
        // Recuperar los resultados
        $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
    
        if ($productos) {
            echo json_encode($productos); // Devuelve en formato JSON
        } else {
            echo json_encode(['mensaje' => 'No se encontraron productos para este paso.']);
        }
    }
    ?>

--- database/database.php
+++ database/database.php
@@ -1,17 +1,14 @@
 <?php
-
-
 $host = "srv1660.hstgr.io";
 $dbname = "u208245273_glowup";
 $username = "u208245273_micaelaa";
 $password = "j2?W7d7Z";
-$respuesta_estado = "";
 
-// Conexión usando PDO
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    
-    echo($respuesta_estado);
+    $respuesta_estado = "Conexión exitosa"; 
+    //echo($respuesta_estado);
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
+?>
